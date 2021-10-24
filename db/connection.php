8b07597edbf4e10b5128de221130e9c303ece965
Constantino <constanedes2001@gmail.com>
2021-10-24 08:39:30 -0300

    Agredados index y conexion a bd

---
+++ db/connection.php
@@ -0,0 +1,28 @@
+<?php
+
+require("config.php");
+
+class Conection {
+
+    public function connect(){
+        try {
+
+            $options = array( 
+                PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+                PDO::ATTR_CASE => PDO::CASE_NATURAL,
+                PDO::ATTR_ORACLE_NULLS => PDO::NULL_EMPTY_STRING
+            );
+
+            $db = new PDO("mysql:host=".HOSTNAME.";port=".PORT.";dbname=".DATABASE.';charset='. CHARSET, USERNAME, PASSWORD, $options);
+
+            return $db;
+        } 
+        catch(Exception $e) {
+            die("Database connection failed: " . $e->getMessage());
+        }
+    }
+}
+
+
+
+?>
---
+++ index.php
@@ -0,0 +1,22 @@
+<?php
+
+include("db/connection.php");
+include("functions.php");
+
+
+/*
+$obj = new Conection();
+$conn = $obj->connect();
+
+$sql = "SELECT * FROM task";
+$result = $conn->prepare($sql);
+
+$result->execute();
+$data = $result->fetchAll(PDO::FETCH_ASSOC);
+
+dump_debug($data);
+*/
+
+
+
+?>
